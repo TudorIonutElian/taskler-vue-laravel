80a2995d6d3437b3cb7d11b43716ac13a4e5d2c3
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-12-04 22:34:17 +0200

    Added tokens and categories table migration

---
+++ database/migrations/2022_12_04_203038_create_tokens_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('token');
+            $table->datetime('created_at');
+            $table->datetime('expiring_on')->nullable();
+            $table->tinyInteger('can_create')->default(0);
+            $table->tinyInteger('can_read')->default(1);
+            $table->tinyInteger('can_update')->default(0);
+            $table->tinyInteger('can_delete')->default(0);
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
