fddf1a812c1f416341010fcb648ff3c9b24e7e92
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-12-04 22:28:32 +0200

    Migrations updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('taskler_user_name');
+            $table->string('taskler_user_email')->unique();
+            $table->timestamp('taskler_user_email_verified_at')->nullable();
+            $table->string('taskler_user_password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps('taskler_user_created_at');
         });
     }
 
